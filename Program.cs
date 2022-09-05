57169336ac8c07766cb9e8ce3222e59d9e7f8cf2
helder <jose.silva2000@etec.sp.gov.br>
2022-09-04 21:42:00 -0300

    resolução da atividade musica

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Meu coração é regador de flor, Te regando amor, cê vai me amar, aah!");
+Console.WriteLine("Meu coração é regador de amor Te regando flor, cê vai me amar, aah!");
+Console.WriteLine("Canção flor de jorge e mateus, data do lançamento 17.02.2014");
